a353a89906836d35c17d91c7449ef22057849072
Yanfer <oscar.yanfer@unmsm.edu.pe>
2022-07-15 11:50:45 -0500

    corrigiendo enlaces

--- src/components/CreateAlumnoComponent.js
+++ src/components/CreateAlumnoComponent.js
@@ -41,7 +41,7 @@ class CreateAlumnoComponent extends Component {
     }
 
     cancel() {
-        this.props.history.push('/reporte-alumno');
+        this.props.history.push('./');
     }
 
     agregarotro() {
@@ -76,7 +76,7 @@ class CreateAlumnoComponent extends Component {
                                 </div>
 
                                 <button className="btn btn-success" onClick={this.saveAlumno} style={{ marginLeft: "330px", marginTop: "10px" }}>Guardar alumno</button>
-                                <button className="btn btn-danger" onClick={this.cancel.bind(this)} style={{ marginLeft: "10px", marginTop: "10px" }}>Cancelar</button>
+                                <a href="./" className="btn btn-danger" style={{ marginLeft: "10px", marginTop: "10px" }}>Cancelar</a>
                                 <button className="btn btn-info" onClick={this.agregarotro.bind(this)} style={{ marginLeft: "10px", marginTop: "10px" }}>Agregar otro alumno</button>
                             </form>
 
--- src/components/ReporteNotasComponent.js
+++ src/components/ReporteNotasComponent.js
@@ -21,27 +21,34 @@ class ReporteNotasComponent extends Component {
 
     render() {
         return (
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th className="head" style={{color: "black"}}>Alumno</th>
-                        <th className="head" style={{color: "black"}}>Curso</th>
-                        <th className="head" style={{color: "black"}}>Promedio final</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        this.state.cursos.map(
-                            curso =>
-                                <tr key={curso.nombre}>
-                                    <td> {curso.nombre} </td>
-                                    <td> {curso.nombrecurso} </td>
-                                    <td> {curso.promedio} </td>
-                                </tr>
-                        )
-                    }
-                </tbody>
-            </table >
+            <div>
+                <div>
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th className="head" style={{color: "black"}}>Alumno</th>
+                                <th className="head" style={{color: "black"}}>Curso</th>
+                                <th className="head" style={{color: "black"}}>Promedio final</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                this.state.cursos.map(
+                                    curso =>
+                                        <tr key={curso.nombre}>
+                                            <td> {curso.nombre} </td>
+                                            <td> {curso.nombrecurso} </td>
+                                            <td> {curso.promedio} </td>
+                                        </tr>
+                                )
+                            }
+                        </tbody>
+                    </table >
+                </div>
+                <div>
+                    <a href="./" className="btn btn-danger" style={{ marginLeft: "10px", marginTop: "10px" }}>Regresar</a>
+                </div>
+            </div>
         )
     }
 }
